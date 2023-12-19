82f01b24c1e778c7495a696ee9b572867a5635ee
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-19 05:31:09 -0600

    Fix validation for 11 digits phone numbers

--- JavaScript-Algorithms-and-DataStructures/telephone-number-validatos.js
+++ JavaScript-Algorithms-and-DataStructures/telephone-number-validatos.js
@@ -2,26 +2,27 @@
 //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/telephone-number-validator
 
 const validatePhoneNumbersLengt = (str)=> {
-    console.log('validating number lengt')
-    //then we filter non numberic elements
-    let temp = []
-    for(let i = 0; i < str.length; i++){
-      if(str.charAt(i).match(/[0-9]/)){
-        temp.push(str.charAt(i))
-      }
+  console.log('validating number lengt')
+  //then we filter non numberic elements
+  let temp = []
+  for(let i = 0; i < str.length; i++){
+    if(str.charAt(i).match(/[0-9]/)){
+      temp.push(str.charAt(i))
     }
-    //then we veryfy that length === 10 or 11.
-    if (temp.length === (10||11) ) return true;
-    console.log('number ->',temp)
-    return false
   }
-  
-  function telephoneCheck(str) {
-    console.log('runing telephoneCheck(',str,')')
-    //validate that numbers should be 10 without country code
-    //or 11 with countrycode.
-    if(validatePhoneNumbersLengt(str) === false) return false;
-    return true;
-  }
-  
-  telephoneCheck("555-555-5555");
+  //then we veryfy that length === 10 or 11.
+  if (temp.length === (10) ) return true;
+  if (temp.length === (11) ) return true;
+  console.log('number ->',temp)
+  return false
+}
+
+function telephoneCheck(str) {
+  console.log('runing telephoneCheck(',str,')')
+  //validate that numbers should be 10 without country code
+  //or 11 with countrycode.
+  if(validatePhoneNumbersLengt(str) === false) return false;
+  return true;
+}
+
+telephoneCheck("555-555-5555");
